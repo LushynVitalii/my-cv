fd40524361a92f9e7265b1c5cb17cd3926878d63
vitertik <cF!%eq34>
2021-04-13 10:55:02 -0400

    header created, but need to play with colours, particles and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Navbar from './components/Navbar'
+import Header from "./components/Header"
+import Particles from "react-particles-js"
 
 function App() {
   return (
+      <>
+      <Particles className='particles-canvas'
+          params={{
+              particles: {
+                  number: {
+                  value: 20,
+                  density: {
+                      enable: true,
+                      value_area: 1300
+              }
+              },
+                  shape: {
+                      type: 'circle',
+                      stroke: {
+                          width: 6,
+                          color: 'gray'
+                      }
+                  }
+          }}} />
       <Navbar />
+      <Header />
+      </>
   );
-}
+};
 
 export default App;
